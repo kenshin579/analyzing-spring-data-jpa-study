ef28f68cbc29f3d882e6334da1b241aee82066c4
kenshin579 <kenshin579@hotmail.com>
2020-10-27 17:32:20 +0900

    [chap02-04] Query 스터디완료

--- src/main/java/me/freelife/springjpaprograming/JpaRunner.java
+++ src/main/java/me/freelife/springjpaprograming/JpaRunner.java
@@ -8,6 +8,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 @Component
 @Transactional
@@ -18,29 +23,28 @@ public class JpaRunner implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		Session session = entityManager.unwrap(Session.class);
-		createPostAndComments(session, 3, 4);
+		//		Session session = entityManager.unwrap(Session.class);
+		//		createPostAndComments(session, 3, 4);
 
-		// JPQL Example
-//		TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post As p", Post.class);
-//		List<Post> posts = query.getResultList();
+		//1. JPQL Example
+		//		TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post As p", Post.class);
+		//		List<Post> posts = query.getResultList();
+		//		posts.forEach(System.out::println);
 
-		// Criteria
-        /*
-        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Post> criteria = builder.createQuery(Post.class);
-        Root<Post> root = criteria.from(Post.class);
-        criteria.select(root);
-
-        List<Post> posts = entityManager.createQuery(criteria).getResultList();
-        */
-
-		//        // Native Query
-		//        List<Post> posts = entityManager
-		//                .createNativeQuery("SELECT * FROM post", Post.class)
-		//                .getResultList();
-		//
+		//2. Criteria
+//		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+//		CriteriaQuery<Post> criteria = builder.createQuery(Post.class);
+//		Root<Post> root = criteria.from(Post.class);
+//		criteria.select(root);
+//
+//		List<Post> posts = entityManager.createQuery(criteria).getResultList();
 //		posts.forEach(System.out::println);
+
+		//3. Native Query
+		List<Post> posts = entityManager
+				.createNativeQuery("SELECT * FROM post", Post.class)
+				.getResultList();
+		posts.forEach(System.out::println);
 	}
 
 	private void createPostAndComments(Session session, int maxPost, int maxComment) {
