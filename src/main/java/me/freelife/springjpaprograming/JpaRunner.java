bda23b739684f8654ce99ff29449cfc06ba7922e
kenshin579 <kenshin579@hotmail.com>
2020-10-27 17:07:52 +0900

    [chap02-04] Fetch 예제 수정

--- src/main/java/me/freelife/springjpaprograming/JpaRunner.java
+++ src/main/java/me/freelife/springjpaprograming/JpaRunner.java
@@ -19,35 +19,34 @@ public class JpaRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         // Post(Parent) Comment(Child) Cascade 테스트 예제
-        /*
-        Post post = new Post();
-        post.setTitle("Spring Data JPA 언제 보나...");
-
-        Comment comment = new Comment();
-        comment.setComment("빨리 보고 싶어요.");
-        post.addComment(comment);
-
-        Comment comment1 = new Comment();
-        comment1.setComment("곧 보여드릴께요.");
-        post.addComment(comment1);
-
-        Session session = entityManager.unwrap(Session.class);
-        session.save(post);
-        */
+//        Post post = new Post();
+//        post.setTitle("Spring Data JPA 언제 보나...");
+//
+//        int max = 10;
+//        Comment comment;
+//
+//        for (int i = 0; i < max; i++) {
+//            comment = new Comment();
+//            comment.setComment(i + " 빨리 보고 싶어요.");
+//            post.addComment(comment);
+//        }
+//
+//        Session session = entityManager.unwrap(Session.class);
+//        session.save(post);
         // EAGER 테스트
         Session session = entityManager.unwrap(Session.class);
-        Post post = session.get(Post.class, 4l);
+        Post savePost = session.get(Post.class, 18l);
         System.out.println("========================");
-        System.out.println(post.getTitle());
+        System.out.println(savePost.getTitle());
 
         // Lazy n+1 테스트
-        post.getComments().forEach(c -> {
+        savePost.getComments().forEach(c -> {
             System.out.println("--------------");
             System.out.println(c.getComment());
         });
 
         // Lazy 테스트
-//        Comment comment = session.get(Comment.class, 5l);
+//        Comment comment = session.get(Comment.class, 19l);
 //        System.out.println("=====================");
 //        System.out.println(comment.getComment());
 //        System.out.println(comment.getPost().getTitle());
