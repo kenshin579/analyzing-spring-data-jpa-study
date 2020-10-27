6a9b68743b759447f1eb40866dff94ac46cb52f4
kenshin579 <kenshin579@hotmail.com>
2020-10-27 17:27:52 +0900

    [chap02-04] sample 데이터 생성 코드작성중

--- src/main/java/me/freelife/springjpaprograming/JpaRunner.java
+++ src/main/java/me/freelife/springjpaprograming/JpaRunner.java
@@ -8,28 +8,24 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.util.List;
 
 @Component
 @Transactional
 public class JpaRunner implements ApplicationRunner {
 
-    @PersistenceContext
-    EntityManager entityManager;
+	@PersistenceContext
+	EntityManager entityManager;
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        // JPQL Example
-        /*
-        TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post As p", Post.class);
-        List<Post> posts = query.getResultList();
-        */
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		Session session = entityManager.unwrap(Session.class);
+		createPostAndComments(session, 3, 4);
 
-        // Criteria
+		// JPQL Example
+//		TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post As p", Post.class);
+//		List<Post> posts = query.getResultList();
+
+		// Criteria
         /*
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Post> criteria = builder.createQuery(Post.class);
@@ -39,11 +35,29 @@ public class JpaRunner implements ApplicationRunner {
         List<Post> posts = entityManager.createQuery(criteria).getResultList();
         */
 
-        // Native Query
-        List<Post> posts = entityManager
-                .createNativeQuery("SELECT * FROM post", Post.class)
-                .getResultList();
+		//        // Native Query
+		//        List<Post> posts = entityManager
+		//                .createNativeQuery("SELECT * FROM post", Post.class)
+		//                .getResultList();
+		//
+//		posts.forEach(System.out::println);
+	}
 
-        posts.forEach(System.out::println);
-    }
+	private void createPostAndComments(Session session, int maxPost, int maxComment) {
+		Post post;
+		Comment comment;
+
+		for (int i = 0; i < maxPost; i++) {
+			post = new Post();
+			post.setTitle(i + " title");
+
+			for (int j = 0; j < maxComment; j++) {
+				comment = new Comment();
+				comment.setComment(i + "_" + j + " command");
+				post.addComment(comment);
+			}
+
+			session.save(post);
+		}
+	}
 }
