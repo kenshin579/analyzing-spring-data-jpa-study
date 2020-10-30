cfea196c48401ec3874cd200b651af748d5900c8
kenshin579 <kenshin579@hotmail.com>
2020-10-30 16:58:15 +0900

    [chap02-04] lombok 적용

--- src/main/java/me/freelife/springjpaprograming/Comment.java
+++ src/main/java/me/freelife/springjpaprograming/Comment.java
@@ -1,11 +1,16 @@
 package me.freelife.springjpaprograming;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import java.util.Date;
 
+@Getter
+@Setter
 @Entity
 public class Comment {
 
@@ -15,48 +20,4 @@ public class Comment {
 
     @ManyToOne
     private Post post;
-
-    private Date created;
-
-    private Integer likeCount = 0;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public Post getPost() {
-        return post;
-    }
-
-    public void setPost(Post post) {
-        this.post = post;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-    public Integer getLikeCount() {
-        return likeCount;
-    }
-
-    public void setLikeCount(Integer likeCount) {
-        this.likeCount = likeCount;
-    }
 }
