2129562eb9fffd12a875f6838a30570db126eb8b
kenshin579 <kenshin579@hotmail.com>
2020-11-04 15:52:59 +0900

    [chap03-05] getter & setter 삭제 - lombok로 자동 생성됨

--- src/main/java/me/freelife/springjpaprograming/Freelife.java
+++ src/main/java/me/freelife/springjpaprograming/Freelife.java
@@ -8,12 +8,4 @@ import lombok.Setter;
 public class Freelife {
 
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
