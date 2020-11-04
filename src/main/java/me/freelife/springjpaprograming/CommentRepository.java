d988f2cde04da74bf584bc203af0edec561f0287
kenshin579 <kenshin579@hotmail.com>
2020-11-04 16:01:36 +0900

    [chap03-05] Repository 메서드 활성화

--- src/main/java/me/freelife/springjpaprograming/CommentRepository.java
+++ src/main/java/me/freelife/springjpaprograming/CommentRepository.java
@@ -1,21 +1,28 @@
 package me.freelife.springjpaprograming;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 public interface CommentRepository extends MyRepository<Comment, Long>{
 
     //Comment 조회
-    //List<Comment> findByCommentContains(String keyword);
+    List<Comment> findByCommentContains(String keyword);
+
     //IgnoreCase 추가
-    //List<Comment> findByCommentContainsIgnoreCase(String keyword);
+    List<Comment> findByCommentContainsIgnoreCase(String keyword);
+
     //likeCount가 특정 숫자보다 높을 때 조건 추가
-    //List<Comment> findByCommentContainsIgnoreCaseAndLikeCountGreaterThan(String keyword, int likeCount);
+    List<Comment> findByCommentContainsIgnoreCaseAndLikeCountGreaterThan(String keyword, int likeCount);
+
     //Comment를 likeCount 높은 순으로 정렬
-    //List<Comment> findByCommentContainsIgnoreCaseOrderByLikeCountDesc(String keyword);
+    List<Comment> findByCommentContainsIgnoreCaseOrderByLikeCountDesc(String keyword);
+
     //Pageable로 동적으로 정렬
-    //Page<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
+//    Page<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
+
     //Stream으로 받아오기
     Stream<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
 }
