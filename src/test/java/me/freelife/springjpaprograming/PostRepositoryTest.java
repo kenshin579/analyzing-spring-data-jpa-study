aefff2fbe4f7bf895432124282356b9ae5e221ec
kenshin579 <kenshin579@hotmail.com>
2020-12-31 15:45:56 +0900

    [chap03-07] 코멘트 추가

--- src/test/java/me/freelife/springjpaprograming/PostRepositoryTest.java
+++ src/test/java/me/freelife/springjpaprograming/PostRepositoryTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataJpaTest //@Transactional 어노테이션이 포함되어 있ㅇ므
 public class PostRepositoryTest {
 
     @Autowired
@@ -19,6 +19,9 @@ public class PostRepositoryTest {
         post.setTitle("hibernate");
         postRepository.save(post);
         postRepository.findMyPost();
+
+//        Hibernate의 경우 ROLLBACK 트랜잭션의 경우 필요없는 Query는 아예 날리지 않음
+//        강제로 flush() 하면 delete Query를 날림
         postRepository.delete(post);
         postRepository.flush();
     }
